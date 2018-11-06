4360a57af86bdd5c486f90e41850fc342bb0225e
smuroy <smuroy@targethr.com.pe>
2018-11-06 11:37:45 -0500

    Cambio de creacion de datasource ya no con libreria de tomcat

--- src/main/java/pe/com/human/servicios/util/ConexionBaseDatos.java
+++ src/main/java/pe/com/human/servicios/util/ConexionBaseDatos.java
@@ -3,19 +3,18 @@ package pe.com.human.servicios.util;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import pe.com.human.servicios.model.ConfiguracionDataSource;
 
 public class ConexionBaseDatos {
 	
 	public static Connection obtenerConexion(ConfiguracionDataSource configuracion) throws SQLException {
-		BasicDataSource datasource = new BasicDataSource();
-		datasource.setDriverClassName(configuracion.getDriverClassName());
-		datasource.setUrl(configuracion.getUrl());
-		datasource.setUsername(configuracion.getUsername());
-		datasource.setPassword(configuracion.getPassword());
-		
-		return datasource.getConnection();
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(configuracion.getDriverClassName());
+        dataSource.setUrl(configuracion.getUrl());
+        dataSource.setUsername(configuracion.getUsername());
+        dataSource.setPassword(configuracion.getPassword());
+		return dataSource.getConnection();
 	}
 }
